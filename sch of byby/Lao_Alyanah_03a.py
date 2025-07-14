f5b6fabf9322b2bf0f711b52a56c5df887fa3fb3
wijii <quitalouie3@gmail.com>
2025-07-15 00:11:47 +0800

    Updated Exe 3A

--- sch of byby/Lao_Alyanah_03a.py
+++ sch of byby/Lao_Alyanah_03a.py
@@ -14,11 +14,13 @@ if x%2 == 0 and y%2 == 0: #checks if the x and y are divisible by 2
 elif x%2 != 0 and y%2 != 0: #checks if the numbers are odd
     print("Both numbers are odd")
 
-elif x%2 == 0 and y%2 !=0 or x%2 !=0 and y%2 == 0: #checks if one number is odd or even
-    if x>y:
-        print(x, "is the Largest even number") #this code runs if the x is greater than y 
-    else:
-        print(y, "Is the largest even number") #this code runs if the y is greater than x
+elif x % 2 == 0 and y % 2 != 0:  # if x is even and y is odd
+    print(x, "is the largest even number") #this code runs if the x is greater than y 
+
+elif x % 2 != 0 and y % 2 == 0:  # if y is even and x is odd
+    print(y, "is the largest even number") #this code runs if the y is greater than x
+
+    
 
 
 
